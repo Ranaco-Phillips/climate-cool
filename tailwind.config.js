8add76eee55209681a6a5291b1384f4031b42ea6
Ranaco-Phillips <63677619+Ranaco-Phillips@users.noreply.github.com>
2024-05-04 10:03:54 -0400

    Update tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,33 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      'almond': '#EAE0D5',
+      'gunmetal': '#22333B',
+      'khaki': '#C6AC8F',
+      'walnut-brown': '#5E503F',
+      'black': '#0A0908',
+      'white': '#FFFFFF',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+        90: '90',
+        100: '100',
       },
     },
   },
